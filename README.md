6545a2aaa956d413fd71ca7932f90b684a019390
Nathan Blaylock <ngblaylock@gmail.com>
2021-11-07 14:22:00 -0700

    About ready for first publish

--- README.md
+++ README.md
@@ -1,14 +1,33 @@
-## Extract GSheet
+# Extract GSheet
 
-This is a prototype for testing out extracting data from a google sheet without going through the API. No keys necessary. This will only read data from a Google sheet provided that you publish and format your document correctly.
+This is a prototype for testing out extracting data from a Google Sheet without going through the API. **No keys necessary!** This script will only read data from a Google Sheet provided that you publish and format your document correctly. Writing data to the Google Sheet through this script is not an option.
 
-This project was inspired by Tabletop, which no longer works due to Google changing their API. It was a fantastic tool while it lasted.
+This project was inspired by [Tabletop](https://github.com/jsoma/tabletop#readme), which no longer works due to Google changing their API. It was a fantastic tool while it lasted. There are other options out there as well, but they seem to have a little more setup than this option.
 
-## Things to Know
+## Features
+
+- Simply add the publicHTML url provided
+- No API Keys required
+- Will extract all tabs in the sheet
+- Transforms column headers into usable keys
+- Each table includes the name of the tab, keys, and data values
+- Clears out columns without a column header
+- Clears out empty rows
+- Transforms 'TRUE' and 'FALSE' to true and false (Boolean)
+- Preserves formatting from document ('$1' does not translate to '1')
+- Extracts page if it is a single tab, or multiple tabs
+
+## Warnings
 
 - This is not thoroughly tested, so I would not recommend this for production, unless maybe your user base is super small and they can let you know when things change.
 - This heavily depends on the output HTML that Google provides. If it changes a little bit, this script will break as well.
+- Checkboxes are not preserved (but you can bind the value to another cell for true/false)
+- Images, charts, comments, etc are not included
+- Some changes may take up to 5 minuts to reflect in the browser
 
 ## To Do
 
-Filter out empty lines (See Bad Example)
+- Make Favicon
+- Minify the Build
+- Documentation Page
+
--- index.js
+++ index.js
@@ -17,46 +17,70 @@ async function FetchHtml(pubHTML) {
 const getTables = function (doc) {
   let tables = [];
   let data = [];
+
+  // Find and Add Tables
   doc.querySelectorAll("#sheets-viewport table").forEach((table) => {
     if (table.textContent) {
       tables.push(table);
     }
   });
+
+  // Setup Table
   tables.forEach((table, tableIndex) => {
     let rows = [];
+    let tab = doc.querySelectorAll("#sheet-menu li")[tableIndex]
     data.push({
-      tab: doc.querySelectorAll("#sheet-menu li")[tableIndex].textContent,
+      tab: tab ? tab.textContent : '',
+      keys: [],
       data: [],
     });
+
+    // Add All Rows
     table.querySelectorAll("tr").forEach((tr) => {
       if (tr.textContent) {
         rows.push(tr);
       }
     });
-    let keys = [];
+
+    // Get Keys
     rows[0].querySelectorAll("td").forEach((key) => {
-      keys.push(
-        key.textContent
-          .replace(/\W+/gi, "_")
-          .replace(/^\d/, "_$&")
-          .toLowerCase()
-      );
+      if (key.textContent) {
+        data[tableIndex].keys.push({
+          name: key.textContent,
+          key: key.textContent
+            .replace(/\W+/gi, "_")
+            .replace(/^\d/, "_$&")
+            .toLowerCase(),
+        });
+      }
     });
+
+    // Extract Data
     for (let x = 0; x < rows.length; x++) {
       if (x != 0) {
         data[tableIndex].data.push({});
         rows[x].querySelectorAll("td").forEach((td, tdIndex) => {
-          let key = keys[tdIndex];
-          let content = td.textContent;
-          if (content && content.toLowerCase() === "true") content = true;
-          else if (content && content.toLowerCase() === "false")
-            content = false;
-          if (key) {
-            data[tableIndex].data[x - 1][key] = td.textContent;
+          if (data[tableIndex].keys[tdIndex]) {
+            let key = data[tableIndex].keys[tdIndex].key;
+            let content = td.textContent;
+            if (content && content.toLowerCase() === "true") content = true;
+            else if (content && content.toLowerCase() === "false")
+              content = false;
+            if (key) {
+              data[tableIndex].data[x - 1][key] = content;
+            }
           }
         });
       }
     }
   });
+
+  // Remove Empty Rows
+  data.forEach((d) => {
+    d.data = d.data.filter((row) => {
+      return !Object.values(row).every((v) => v === "");
+    });
+  });
+
   return data;
 };
