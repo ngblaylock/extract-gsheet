0c262ad37dc9a5515cad06d84af11a55e8be07a9
Nathan Blaylock <ngblaylock@gmail.com>
2021-11-09 23:04:02 -0700

    Started 11ty project for documentation

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const pluginSass = require("eleventy-plugin-sass");
+const env = process.env.ELEVENTY_ENV;
+
+module.exports = (eleventyConfig) => {
+  console.log(env);
+  // Copy our static assets to the output folder
+  eleventyConfig.addPassthroughCopy("./11ty-site/assets");
+  eleventyConfig.addPassthroughCopy({"dist": "js"})
+  eleventyConfig.addPlugin(pluginSass, {outputDir: env == 'dev' ? "dev/styles" : 'docs/styles', remap: true});
+
+  return {
+    dir: {
+      input: "./11ty-site/pages",
+      output: env == 'dev' ? 'dev' : "docs",
+    },
+  };
+};
---
+++ docs/js/extract-gsheet.js
@@ -0,0 +1,96 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.extractGSheet = factory());
+})(this, (function () { 'use strict';
+
+  const extractGSheet = async function (url) {
+      let page = await FetchHtml(url).then((text) => {
+        return text;
+      });
+      var doc = new DOMParser().parseFromString(page, "text/html");
+      return {
+        title: doc.querySelector("#doc-title .name").textContent,
+        tables: getTables(doc),
+      };
+    };
+
+    async function FetchHtml(url) {
+      let response = await fetch(url);
+      return await response.text(); // Returns it as Promise
+    }
+
+    const getTables = function (doc) {
+      let tables = [];
+      let data = [];
+
+      // Find and Add Tables
+      doc.querySelectorAll("#sheets-viewport table").forEach((table) => {
+        if (table.textContent) {
+          tables.push(table);
+        }
+      });
+
+      // Setup Table
+      tables.forEach((table, tableIndex) => {
+        let rows = [];
+        let tab = doc.querySelectorAll("#sheet-menu li")[tableIndex];
+        data.push({
+          tab: tab ? tab.textContent : "",
+          keys: [],
+          data: [],
+        });
+
+        // Add All Rows
+        table.querySelectorAll("tr").forEach((tr) => {
+          if (tr.textContent) {
+            rows.push(tr);
+          }
+        });
+
+        // Get Keys
+        rows[0].querySelectorAll("td").forEach((key) => {
+          if (key.textContent) {
+            data[tableIndex].keys.push({
+              name: key.textContent,
+              key: key.textContent
+                .replace(/\W+/gi, "_")
+                .replace(/^\d/, "_$&")
+                .toLowerCase(),
+            });
+          }
+        });
+
+        // Extract Data
+        for (let x = 0; x < rows.length; x++) {
+          if (x != 0) {
+            data[tableIndex].data.push({});
+            rows[x].querySelectorAll("td").forEach((td, tdIndex) => {
+              if (data[tableIndex].keys[tdIndex]) {
+                let key = data[tableIndex].keys[tdIndex].key;
+                let content = td.textContent;
+                if (content && content.toLowerCase() === "true") content = true;
+                else if (content && content.toLowerCase() === "false")
+                  content = false;
+                if (key) {
+                  data[tableIndex].data[x - 1][key] = content;
+                }
+              }
+            });
+          }
+        }
+      });
+
+      // Remove Empty Rows
+      data.forEach((d) => {
+        d.data = d.data.filter((row) => {
+          return !Object.values(row).every((v) => v === "");
+        });
+      });
+
+      return data;
+    };
+
+  return extractGSheet;
+
+}));
