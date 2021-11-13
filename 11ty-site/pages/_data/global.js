e3962535ceb83104625b4230c91d3acaf3dad52e
Nathan Blaylock <ngblaylock@gmail.com>
2021-11-12 23:07:00 -0700

    Update documentation

--- 11ty-site/pages/_data/global.js
+++ 11ty-site/pages/_data/global.js
@@ -1,2 +1,9 @@
-module.exports = {
+const dayjs = require('dayjs');
+
+module.exports = function(){
+  return {
+    env: process.env.ELEVENTY_ENV,
+    copyrightYear: new Date().getFullYear(),
+    today: dayjs().format('MMMM D, YYYY')
+  }
 }
