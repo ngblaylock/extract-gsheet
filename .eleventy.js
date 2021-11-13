02803f9c90e57908d30d1857644e76c61dd6551d
Nathan Blaylock <ngblaylock@gmail.com>
2021-11-13 10:04:31 -0700

    Add logos, minor style changes, build

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ const env = process.env.ELEVENTY_ENV;
 module.exports = (eleventyConfig) => {
   console.log(env);
   // Copy our static assets to the output folder
-  eleventyConfig.addPassthroughCopy("./11ty-site/assets");
+  eleventyConfig.addPassthroughCopy({"./11ty-site/assets": "/"});
   eleventyConfig.addPassthroughCopy({ dist: "js" });
   eleventyConfig.addPlugin(pluginSass, {
     outputDir: env == "dev" ? "dev/styles" : "docs/styles",
