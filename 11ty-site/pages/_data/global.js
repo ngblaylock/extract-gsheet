006946efa9d452328f2735403f626d2c64f34527
Nathan Blaylock <ngblaylock@gmail.com>
2021-11-13 20:08:25 -0700

    Add seo

--- 11ty-site/pages/_data/global.js
+++ 11ty-site/pages/_data/global.js
@@ -3,6 +3,7 @@ const dayjs = require('dayjs');
 module.exports = function(){
   return {
     env: process.env.ELEVENTY_ENV,
+    baseURL: "https://ngblaylock.github.io",
     copyrightYear: new Date().getFullYear(),
     today: dayjs().format('MMMM D, YYYY')
   }
